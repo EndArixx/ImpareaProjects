39c7a03f5fa5c6388bc3c62597710332ecd3a90f
John Mark Stanley <endarixx@users.noreply.github.com>
2022-12-20 16:49:26 -0500

    cleans up methods
    Adds Slicing to get transformations

--- Collect Episodes.py
+++ Collect Episodes.py
@@ -6,10 +6,12 @@ from PIL import Image
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-i", "--insta", action="store_true", dest="insta", default=False)
+parser.add_option("-t", "--transform", action="store_true", dest="transform", default=False)
 (options, args) = parser.parse_args()
 
 file_type = '.png'
 insta_folder_name = 'Insta'
+transform_folder_name = 'Transforms'
 uncensored_folder_name = '(U)'
 parent_dir = pathlib.Path(__file__).parent.resolve()
 pages_dir = os.path.abspath(os.path.join(parent_dir, '..\\Pages\\'))
@@ -24,40 +26,26 @@ class coordinate:
 		self.bottom = bottom
 
 
-Overrides = {
-	#coordinate(left, top, right, bottom)
-	"004P2": coordinate(0,1200,1600,2800),
-	"004P3": coordinate(0,2200,1600,3800),
-	"004P4": coordinate(0,3200,1600,4800),
-	"007P1": coordinate(0,0,1600,1600),
-	"007P2": coordinate(0,1000,1600,2600),
-	"007P3": coordinate(0,2200,1600,3800),
-	"007P4": coordinate(0,3200,1600,4800),
-	"009P1": coordinate(0,0,1600,1600),
-	"009P3": coordinate(0,2200,1600,3800),
-	"009P4": coordinate(0,3200,1600,4800),
-	"012P2": coordinate(0,1000,1600,2600),
-	"012P3": coordinate(0,2200,1600,3800),
-}
-
-doubles = [
-	"004P3",
-	"007P3",
-	"014P3",
+transforms = [
+	"004P2",
+	"007P2",
+	"014P2",
 	"036P1",
-	"036P3",
+	"036P2",
 	"041P1",
-	"041P3",
+	"041P2",
 	"046P1",
-	"046P3",
-	"051P3",
-	"052P3",
+	"046P2",
+	"051P2",
+	"052P2",
 ]
 
 #delete existing
 for remove_path in remove_files:
 	print('deleting:{}'.format(remove_path.split('\\')[-1]))
-	if not remove_path.__contains__('\\'+insta_folder_name+'\\') or options.insta:
+	insta = not remove_path.__contains__('\\'+insta_folder_name+'\\') or options.insta
+	transform = not remove_path.__contains__('\\'+transform_folder_name+'\\') or options.transform
+	if insta and transform: 
 		os.remove(remove_path)
 
 def if_not_exist_make_folder(path):
@@ -94,43 +82,11 @@ def copy_stuff(files):
 		if image_destination != 'ERROR':
 			shutil.copy(image_path,image_destination)
 
-def Slice_for_Instagram(instafiles, recreate = False):
-	if_not_exist_make_folder(insta_folder_name)
-	for image_path in instafiles:
-		image_path_split = image_path.split('\\')
-		image_dir = image_path_split[-2]
-		image_name = image_path_split[-1]
-		image = Image.open(image_path)
-		if "{}{}".format(image_dir,file_type) == image_name:
-			x, height = image.size
-			y = 1200
-			gap = (x-y)/2
-			panels = []
-			for i in range(height//y):
-				name = f"{image_name[0:3]}P{i+1}"
-				file = os.path.join(parent_dir, insta_folder_name, f"{name}.png")
-				if name in doubles:
-					s = name.split('.')
-					doubleFile = os.path.join(parent_dir, insta_folder_name, f"{name}D.png")
-					panel = image.crop((0,   i*y,  x, (i+2)*y))
-					panel = panel.crop((-400,   0, x+400,2400))
-					print(f'Saving DOUBLE:{doubleFile}')
-					panel.save(doubleFile)
-				if name in Overrides.keys():
-					ex = Overrides[name]
-					panel = image.crop((ex.left, ex.top, ex.right, ex.bottom))
-					print(f'Saving CUSTOM:{file}')
-					panel.save(file)
-				elif not os.path.exists(file) or recreate:
-					#crop(left, top, right, bottom)
-					panel = image.crop((0,   i*y,  x, (i+1)*y))
-					panel = panel.crop((0,   -gap, x, y+gap))
-					print(f'Saving:{file}')
-					panel.save(file)
 
-def Slice_for_Instagram_V2(instafiles, recreate = False):
-	if_not_exist_make_folder(insta_folder_name)
-	for image_path in instafiles:
+def Slice(files,folder, instagram=False):
+	print("Creating Instagram Files")
+	if_not_exist_make_folder(folder)
+	for image_path in files:
 		image_path_split = image_path.split('\\')
 		image_dir = image_path_split[-2]
 		image_name = image_path_split[-1]
@@ -138,21 +94,25 @@ def Slice_for_Instagram_V2(instafiles, recreate = False):
 		if "{}{}".format(image_dir,file_type) == image_name:
 			x, height = image.size
 			y = 2400
-			gap = (x-y)/2
-			panels = []
 			for i in range(height//y):
 				name = f"{image_name[0:3]}P{i+1}"
-				file = os.path.join(parent_dir, insta_folder_name, f"{name}.png")
-				if not os.path.exists(file) or recreate:
+				file = os.path.join(parent_dir, folder, f"{name}.png")
+				if instagram or name in transforms:
 					panel = image.crop((0, i*y,  x, (i+2)*y))
-					panel = panel.crop((-400, 0, x+400,2400))
+					if instagram:
+						panel = panel.crop((-400, 0, x+400,2400))
+					else:
+						panel = panel.crop((0, 0, x,2400))
 					print(f'Saving:{file}')
 					panel.save(file)
+
 
 def Execute():
 	copy_stuff(pages_files)
 	if options.insta:
-		Slice_for_Instagram_V2(pages_files)
+		Slice(pages_files,insta_folder_name, True)
+	if options.transform:
+		Slice(pages_files, transform_folder_name)
 
 if __name__=="__main__":
 	Execute()
