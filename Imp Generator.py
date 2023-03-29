3ae197d4216ab143376146953db280b99aa4edd9
Jaki Rose <EndArixx@users.noreply.github.com>
2023-03-29 13:57:37 -0400

    Adds new GUI
    Adds tkGui
    Adds ability to export imps to a .CSV

--- Imp Generator.py
+++ Imp Generator.py
@@ -1,13 +1,15 @@
 from operator import truediv
 from random import *
 import random
+import tkinter
+from tkinter import *
+from os.path import exists
 
-print("Generating Imp!")
 def openfile(str): return open(str, 'r').read().split('\n')
-flavors = openfile("Imparea/ImpareaProjects/data/flavors.csv")
-adjectives = openfile("Imparea/ImpareaProjects/data/adjectives.csv")
-nouns = openfile("Imparea/ImpareaProjects/data/nouns.csv")
-colors = openfile("Imparea/ImpareaProjects/data/colors.csv")
+flavors = openfile("data/flavors.csv")
+adjectives = openfile("data/adjectives.csv")
+nouns = openfile("data/nouns.csv")
+colors = openfile("data/colors.csv")
 alphabet = list("abcdefghijklmnopqrstuvwxyz")
 
 def Get_Pronouns(): 
@@ -49,6 +51,73 @@ def Generate_An_Imp():
     color = Get_Color()
     return Imp(name, transform, pronouns, color)
 
-imp = Generate_An_Imp()
-print(imp)
-input("Press any key to close.")
+
+
+def UI():
+    global imp
+    imp = Generate_An_Imp()
+    top = tkinter.Tk()
+    top.title('Imp Generator')
+    textWidth = 45
+    labelPaddingY = (10,0) 
+    paddingX = 10
+
+    def regenImp():
+        global imp 
+        imp = Generate_An_Imp()
+        textName.delete(1.0,END)
+        textName.insert(tkinter.END, imp.name)
+        textTransform.delete(1.0,END)
+        textTransform.insert(tkinter.END, imp.transform)
+        textPronouns.delete(1.0,END)
+        textPronouns.insert(tkinter.END, imp.pronouns)
+        textColor.delete(1.0,END)
+        textColor.insert(tkinter.END, imp.color)
+        buttonExport["state"] = "normal"
+
+    def exportImp():
+        exportPath = 'Imps.csv'
+        if not exists(exportPath):
+            export = open(exportPath, 'w+')
+            export.write("Name,Transform,Pronouns,Color\n")
+        else:
+            export = open(exportPath, 'a+')    
+        export.write(f"{imp.name},{imp.transform},{imp.pronouns},{imp.color}\n")
+        export.close()
+        buttonExport["state"] = "disabled"
+        
+
+
+    labelName = Label(top, text="Name")
+    labelName.pack(pady=labelPaddingY)
+    textName = Text(top, height = 1, width = textWidth)
+    textName.pack(padx=paddingX)
+
+    labelTransform = Label(top, text="Transform")
+    labelTransform.pack(pady=labelPaddingY)
+    textTransform = Text(top, height = 1, width = textWidth)
+    textTransform.pack(padx=paddingX)
+
+    labelPronouns = Label(top, text="Pronouns")
+    labelPronouns.pack(pady=labelPaddingY)
+    textPronouns = Text(top, height = 1, width = textWidth)
+    textPronouns.pack(padx=paddingX)
+
+    labelColor = Label(top, text="Color")
+    labelColor.pack(pady=labelPaddingY)
+    textColor = Text(top, height = 1, width = textWidth)
+    textColor.pack(padx=paddingX)
+
+    buttonRegen = tkinter.Button(top, text ="Regenerate", command = regenImp)
+    buttonRegen.pack(pady=10,padx=10,in_=top, side=LEFT)
+    buttonExport = tkinter.Button(top, text ="Export Imp", command = exportImp)
+    buttonExport.pack(pady=10,padx=10,in_=top, side=LEFT)
+
+
+    textName.insert(tkinter.END, imp.name)
+    textTransform.insert(tkinter.END, imp.transform)
+    textPronouns.insert(tkinter.END, imp.pronouns)
+    textColor.insert(tkinter.END, imp.color)
+
+    top.mainloop()
+UI()
